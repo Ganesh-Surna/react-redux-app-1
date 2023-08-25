d893c4ca579913e168946cdf15072a03cd10b5a3
Ganesh Surna <surnaganesh123@gmail.com>
2023-08-25 16:43:10 +0530

    v1.2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,79 +1,92 @@
 import classes from './Counter.module.css';
-// import { useDispatch, useSelector } from 'react-redux';
-import { Component } from "react";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
+import { counterActions } from '../store';
 
-class Counter extends Component{
+const Counter = () => {
 
-  handleIncrement(){
-    this.props.incrementFun();
-  }
+  const dispatch=useDispatch();
 
-  handleDecrement(){
-    this.props.decrementFun();
-  }
+  const count=useSelector((state)=>state.count);
+  const isShowCounter=useSelector((state)=>state.isShowCounter);
 
-  toggleCounterHandler(){
+  const handleIncrement=()=>{
+    dispatch(counterActions.increment());  //{type:"some_unique_id"}
+  };
 
-  }
+  const handleDecrement=()=>{
+    dispatch(counterActions.decrement());  //{type:"some_unique_id"}
+  };
 
-  render(){
-    return (
-          <main className={classes.counter}>
-            <h1>Redux Counter</h1>
-            <div className={classes.value}>{this.props.count}</div>
-            <div>
-            <button onClick={this.handleIncrement.bind(this)}>Increase</button>
-            <button onClick={this.handleDecrement.bind(this)}>Decrease</button>
-            </div>
-            <button onClick={this.toggleCounterHandler.bind(this)}>Toggle Counter</button>
-          </main>
-    );
-  }
+  const handleIncreaseBy=()=>{
+    dispatch(counterActions.incrementBy(10)); //{type:"some_unique_id", payload:10}
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleShow())    //{type:"some_unique_id"}
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {isShowCounter && <div className={classes.value}>{count}</div>}
+      <div>
+      <button onClick={handleIncrement}>Increase</button>
+      <button onClick={handleDecrement}>Decrease</button>
+      <button onClick={handleIncreaseBy}>Increase By 10</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
+
+
+
+// import { Component } from "react";
+// import { connect } from "react-redux";
+
+// class Counter extends Component{
+
+//   handleIncrement(){
+//     this.props.incrementFun();
+//   }
+
+//   handleDecrement(){
+//     this.props.decrementFun();
+//   }
+
+//   toggleCounterHandler(){
+
+//   }
+
+//   render(){
+//     return (
+//           <main className={classes.counter}>
+//             <h1>Redux Counter</h1>
+//             <div className={classes.value}>{this.props.counter1}</div>
+//             <div>
+//             <button onClick={this.handleIncrement.bind(this)}>Increase</button>
+//             <button onClick={this.handleDecrement.bind(this)}>Decrease</button>
+//             </div>
+//             <button onClick={this.toggleCounterHandler.bind(this)}>Toggle Counter</button>
+//           </main>
+//     );
+//   }
   
 
-}
+// }
 
-const mapStateToProps=(state)=>{
-  return {count:state.count};
-};
-
-const mapDispatchToPros=(dispatch)=>{
-  return {
-    incrementFun:()=>{ return dispatch({type:"increment"}); },
-    decrementFun:()=>{ return dispatch({type:"decrement"}); },
-  };
-};
-
-export default connect(mapStateToProps,mapDispatchToPros)(Counter);
-
-// const Counter = () => {
-
-//   const dispatch=useDispatch();
-
-//   const count=useSelector((state)=>state.count);
-
-//   const handleIncrement=()=>{
-//     dispatch({type:"increment"});
-//   };
-
-//   const handleDecrement=()=>{
-//     dispatch({type:"decrement"});
-//   };
-
-//   const toggleCounterHandler = () => {};
-
-//   return (
-//     <main className={classes.counter}>
-//       <h1>Redux Counter</h1>
-//       <div className={classes.value}>{count}</div>
-//       <div>
-//       <button onClick={handleIncrement}>Increase</button>
-//       <button onClick={handleDecrement}>Decrease</button>
-//       </div>
-//       <button onClick={toggleCounterHandler}>Toggle Counter</button>
-//     </main>
-//   );
+// const mapStateToProps=(state)=>{
+//   return {counter1:state.count};
 // };
 
-// export default Counter;
+// const mapDispatchToPros=(dispatch)=>{
+//   return {
+//     incrementFun:()=>{ return dispatch({type:"increment"}); },
+//     decrementFun:()=>{ return dispatch({type:"decrement"}); },
+//   };
+// };
+
+// export default connect(mapStateToProps,mapDispatchToPros)(Counter);
+
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,54 @@
-// const redux = require("redux"); this is in node
-import {createStore} from "redux";
+import { createSlice,configureStore } from "@reduxjs/toolkit";
 
+const initialState={count:0, isShowCounter:true};
 
-const counterReducer=(state={count:0},action)=>{
-    if(action.type==="increment"){
-        return {count:state.count+1};
-    }
-    if(action.type==="decrement"){
-        return {count:state.count-1};
-    }
-    return state;
-};
+const counterSlice=createSlice({
+    name:"counter",
+    initialState,
+    reducers:{
+        increment(state){
+            state.count++;
+        },
+        incrementBy(state,action){
+            state.count+=action.payload;
+        },
+        decrement(state){
+            state.count--;
+        },
+        toggleShow(state){
+            state.isShowCounter=!state.isShowCounter;
+        },
+    },
+});
 
-const store=createStore(counterReducer);
+const store=configureStore({
+    reducer: counterSlice.reducer,
+});
+
+export const counterActions=counterSlice.actions;
 
 export default store;
+
+
+
+
+
+
+
+
+
+// import {createStore} from "redux";
+
+// const counterReducer=(state={count:0},action)=>{
+//     if(action.type==="increment"){
+//         return {count:state.count+1};
+//     }
+//     if(action.type==="decrement"){
+//         return {count:state.count-1};
+//     }
+//     return state;
+// };
+
+// const store=createStore(counterReducer);
+
+// export default store;
